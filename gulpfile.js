417cec6f08e707bd566c2bbff09e8cd541b35bd4
Laiba Choudhry <laiba@hotmail.nl>
2022-04-05 14:38:41 +0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,19 @@
 const gulp = require('gulp');
 const cleanCSS = require('gulp-clean-css');
  
-gulp.task('minify-css', () => {
-  return gulp.src('/public/css/sass')
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('dist'));
-});
+// gulp.task('minify-css', () => {
+//   return gulp.src('/public/css/sass')
+//     .pipe(cleanCSS({compatibility: 'ie8'}))
+//     .pipe(gulp.dest('dist'));
+// });
  
 gulp.task('minify-css', () => {
-  return gulp.src('/public/css/sass')
+  return gulp.src('/public/css/sass/*')
     .pipe(cleanCSS({debug: true}, (details) => {
       console.log(`${details.name}: ${details.stats.originalSize}`);
       console.log(`${details.name}: ${details.stats.minifiedSize}`);
     }))
   .pipe(gulp.dest('dist'));
 }); 
+
+//bron: https://www.npmjs.com/package/gulp-clean-css
